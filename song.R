a302c5dc3bd96378b2795d45cf924ff5b67b3cdb
vinayaklal98 <vinayaklal98@gmail.com>
2020-09-15 23:27:54 +0530

    song.r

---
+++ song.R
@@ -0,0 +1,12 @@
+artist <- "Skylar Grey"
+#Name of Artist
+released <- "14th December 2018"
+#date when it was released
+recorded <- 2018
+#year when it was recorded
+length <- 3.16
+#length of song in minutes.seconds
+label <- c("WaterTower Music" , "Ingroove")
+#Music Industries 
+producer <- c("Holly Hafermann", "Elliott Taylor", "Rupert Gregson-Williams")
+#Name of producers
